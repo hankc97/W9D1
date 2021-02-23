8f9929c2f26645326655434ddaf2db69fb1d693e
Lijun <gan.lijun.glj@gmail.com>
2021-02-22 23:06:30 -0500

    push

--- flappybird/src/bird.js
+++ flappybird/src/bird.js
@@ -3,16 +3,26 @@ export default class Bird {
         this.dimension = dimension;
         this.heightOfBird = 320;
         this.lengthOfBird = 160;
+        this.velocity = 1 + 0.5;
     }
 
     drawBird(ctx) {
         ctx.fillStyle = "yellow";
         ctx.fillRect(this.lengthOfBird, this.heightOfBird, 40, 30);
+    
     }
 
     animate(ctx) {
+        this.move();
         this.drawBird(ctx);
     }
 
+    move(){
+        this.heightOfBird += this.velocity;
+    }
+
+    flap(){
+        this.velocity = -8
+    }
 
 }
